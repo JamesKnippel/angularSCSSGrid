fe88b7f144156f617baab447222677cb9b35e183
James Knippel <jamesknippel@gmail.com>
2018-01-08 18:01:27 -0800

    implicit grid component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { ContainerComponent } from './container/container.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
